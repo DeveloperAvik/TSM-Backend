1e097360ec2b7cb10d53e43a047f4948fe61140d
root <avikd906@proton.me>
2025-09-06 06:42:39 +0530

    Adding password and fix bugs for email password based user registration apis

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,14 +1,23 @@
-import { IUser } from "./user.interface";
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import httpStatus from 'http-status-codes';
 
 const createUser = async (payload: Partial<IUser>) => {
-    const { name, email } = payload
-    const user = await User.create({ name, email });
+    const { email, ...rest } = payload
 
-    return {
-        data: user,
+    const isUserExist = await User.findOne({ email });
 
+    if (isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User already Exist")
     }
+
+
+    const authProvider: IAuthProvider = { provider: "credentials", providerId: email! }
+
+    const user = await User.create({ email, auths: [authProvider], ...rest });
+
+    return { user }
 }
 
 const getAllUsers = async () => {

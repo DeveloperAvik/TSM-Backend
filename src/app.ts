9d0e4c2b9bb12456be12c4d3b871e050eab6741f
root <avikd906@proton.me>
2025-09-09 15:46:36 +0530

    configuring Passport Js for backend and Google auth config

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
 import express, { NextFunction, Request, Response } from "express";
-import { UserRoutes } from "./app/modules/user/user.route";
 import cors from "cors";
 import { router } from "./app/routes";
-import { success } from "zod";
-import { envVars } from "./app/config/env";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
-import httpStatus from 'http-status-codes';
 import notFound from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session"
 
 const app = express();
 
+app.use(expressSession({
+    secret:"Your Secret",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(cookieParser())
 app.use(express.json());
 app.use(cors())
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -13,11 +13,16 @@ interface EnvVars {
     SUPER_ADMIN_PASS: string;
     JWT_REFREESH_SECRET: string;
     JWT_REFREESH_EXPIRED: string;
+    GOOGLE_CLIENT_SECRET: string,
+    GOOGLE_CLIENT_ID: string,
+    GOOGLE_CALLBACK_URL: string,
+    EXPRESS_SESSION_SECRET: string,
+    FRONTEND_URL: string,
 }
 
 const loasEnvVars = (): EnvVars => {
 
-    const requiredVars: string[] = ['NODE_ENV', 'PORT', 'MONGODB_URL', 'BCRYPT_SALT_ROUND', 'JWT_ACCESS_SECRET', 'JWT_ACCESS_EXPIRES', 'JWT_REFREESH_SECRET', 'JWT_REFREESH_EXPIRED'];
+    const requiredVars: string[] = ['NODE_ENV', 'PORT', 'MONGODB_URL', 'BCRYPT_SALT_ROUND', 'JWT_ACCESS_SECRET', 'JWT_ACCESS_EXPIRES', 'JWT_REFREESH_SECRET', 'JWT_REFREESH_EXPIRED', 'GOOGLE_CLIENT_SECRET', 'GOOGLE_CLIENT_ID', 'GOOGLE_CALLBACK_URL', 'EXPRESS_SESSION_SECRET', 'FRONTEND_URL'];
 
     requiredVars.forEach((varName) => {
         if (!process.env[varName]) {
@@ -36,6 +41,11 @@ const loasEnvVars = (): EnvVars => {
         SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
         JWT_REFREESH_SECRET: process.env.JWT_REFREESH_SECRET as string,
         JWT_REFREESH_EXPIRED: process.env.JWT_REFREESH_EXPIRED as string,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string ,
+        GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string ,
+        EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string ,
+        FRONTEND_URL: process.env.FRONTEND_URL as string,
     };
 };
 
